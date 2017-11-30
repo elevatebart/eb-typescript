efa02a0fd8a595161c041c461b0411d38e3ff923
Bart Ledoux <bart.ledoux@enablon.com>
2017-11-29 18:11:27 -0600

    add dependency to giveEnergy

--- src/puma.ts
+++ src/puma.ts
@@ -1,11 +1,23 @@
 import { Animal } from "./animal";
 
+export interface IEdible {
+    giveEnergy(): number;
+}
 export class Puma extends Animal {
-    constructor() {
+    private energy: number = 0;
+    private readonly food: IEdible;
+    constructor(food: IEdible = {giveEnergy() {
+            return 8;
+        },
+    }) {
         super({
             name: "Big Cat",
         });
+        this.food = food;
         console.log(`puma created ${this.name}`); // if you move this before super() compiler will cry
+    }
+    public eat() {
+        this.food.giveEnergy();
     }
     public shout(): void {
         this.head.open();
